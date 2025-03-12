264b144d5317e3b31e391ea71bd24cdf553b6525
Arjun Manjunath <dev.arjunmnath@gmail.com>
2025-03-12 22:31:26 +0530

    padding update

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        className={wendyOne.className}
+        className={`${wendyOne.className} pb-4`}
       >
         <Toaster
           position="top-left"
